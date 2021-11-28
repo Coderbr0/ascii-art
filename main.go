d4441cca8200a35c2f330b6a7fe80cc1beda3bbe
Coderbro <mo.abbaszadi@gmail.com>
2021-11-28 17:30:57 +0000

    go-reloaded

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
 func main() {}
+
+
+/*
+package main
+
+import "fmt"
+
+func main() {
+m := make(map[string]int)
+m["James"] = 42
+m["Amy"] = 16
+fmt.Println(m["Amy"], m["James"])
+delete(m, "James")
+_, ok := m["James"] // Checking if item exists in a map
+fmt.Println(ok)
+fmt.Println(m["James"])
+}
+
+Alternative syntax:
+
+
+package main
+
+import "fmt"
+
+func main() {
+m := map[string]int{
+"James": 42,
+"Amy":   16}
+fmt.Println(m["Amy"])
+delete(m, "James")
+fmt.Println(m["James"])
+}
+*/
